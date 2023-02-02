7e42964605ac3b99b7e1adc03fcb9a61ef00bb77
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-02-02 18:44:13 +0530

    Added toggle Like Api

--- src/index.js
+++ src/index.js
@@ -30,12 +30,12 @@ app.listen(3000, async () => {
     await connect();
     console.log('MongoDB Connected');
 
-     const userRepo = new UserRepository();
-    const tweetRepo = new TweetRepository();
-    const tweets = await tweetRepo.getAll(0, 10);
-    const users = await tweetRepo.getAll();
-    const likeService = new LikeService();
-    await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
+    //  const userRepo = new UserRepository();
+    // const tweetRepo = new TweetRepository();
+    // const tweets = await tweetRepo.getAll(0, 10);
+    // const users = await tweetRepo.getAll();
+    // const likeService = new LikeService();
+    // await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
 
 
 
